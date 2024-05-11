82c95ed5b3451e564ab803ac9184de02ffdef37b
Dean Mazlish <96462172+DeanMazlish@users.noreply.github.com>
2024-05-11 16:44:35 +0000

    server change

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.get('/Meteor', (req, res) => {
 
 
 // Listen on the configured port
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
